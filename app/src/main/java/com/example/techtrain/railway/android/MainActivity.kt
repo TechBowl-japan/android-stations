1cc5d907ab3d975fbecd00287cb32eab9b16a55b
rintarotajima <rintarotajima@gmail.com>
2025-08-07 16:07:34 +0900

    Android -Kotlin初級編05

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.text).text = "Hello MainActivity"
+        findViewById<TextView>(R.id.text).text = getString(R.string.hello_main_activity)
     }
 }
